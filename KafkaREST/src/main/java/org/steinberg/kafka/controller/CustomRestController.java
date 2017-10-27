6ad39f6a509fbd934876d3dda7e9a27f49b21db3
SbgMartin <sbg.martin@gmail.com>
2017-10-27 11:58:20 +0200

    docker-compose to version 3.3, minor changes regarding docker volumes internal als external

--- KafkaREST/src/main/java/org/steinberg/kafka/controller/CustomRestController.java
+++ KafkaREST/src/main/java/org/steinberg/kafka/controller/CustomRestController.java
@@ -1,5 +1,7 @@
 package org.steinberg.kafka.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,27 +12,33 @@ import org.steinberg.kafka.producer.KafkaProducer;
 import org.steinberg.kafka.storage.MessageStorage;
 
 @RestController
-@RequestMapping(value="/steini/kafka")
+@RequestMapping(value = "/steini/kafka")
 public class CustomRestController {
-	
+
+	private static final Log LOG = LogFactory.getLog(CustomRestController.class);
+
 	@Autowired
 	KafkaProducer producer;
-	
+
 	@Autowired
 	MessageStorage storage;
-	
-	@GetMapping(value="/producer")
-	public String producer(@RequestParam("data")String data){
+
+	@GetMapping(value = "/producer")
+	public String producer(@RequestParam("data") String data) {
+		LOG.debug("Calling Apache Kafka with " + data);
+
 		producer.send(data);
-		
+
+		LOG.debug("Sent");
+
 		return "Done";
 	}
-	
-	@GetMapping(value="/consumer")
-	public String getAllRecievedMessage(){
+
+	@GetMapping(value = "/consumer")
+	public String getAllRecievedMessage() {
 		String messages = storage.toString();
 		storage.clear();
-		
+
 		return messages;
 	}
 }
